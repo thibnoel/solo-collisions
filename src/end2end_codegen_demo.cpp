c66b434f653bbacbc4005d26ee211cebbaf10466
Thibault <thb.noel@gmail.com>
2020-05-25 10:11:45 +0200

    c++ : draft test against FCL

--- src/end2end_codegen_demo.cpp
+++ src/end2end_codegen_demo.cpp
@@ -24,38 +24,51 @@ int main(int argc, char* argv[])
     // Load Solo 12 model
         // Setup URDF path
     const std::string urdf_filename = "/opt/openrobots/share/example-robot-data/robots/solo_description/robots/solo12.urdf";
+    const std::string robots_model_path = "/opt/openrobots/share/example-robot-data/robots";// PINOCCHIO_MODEL_DIR + std::string("/others/robots/"); 
 
         // Load and build the URDF model
     Model rmodel;
+    GeometryModel gmodel;
     pinocchio::urdf::buildModel(urdf_filename,rmodel);
+    pinocchio::urdf::buildGeom(rmodel, urdf_filename, pinocchio::COLLISION, gmodel, robots_model_path);
         // Generate model data
     Data rdata(rmodel); 
 
     // Get frames indices from the model
-    int fl_upper_leg = (int)rmodel.getFrameId(frame1Name);
-    int hr_lower_leg = (int)rmodel.getFrameId(frame2Name);
+    int frameInd1 = (int)rmodel.getFrameId(frame1Name);
+    int frameInd2 = (int)rmodel.getFrameId(frame2Name);
 
     // Define capsule parameters
         // Geometry
     ADScalar capsLength;
     ADScalar capsRadius;
+    double fcl_capsRad = 0.2;
+    double fcl_capsLen = 0.02;
     capsLength = 0.2;
     capsRadius = 0.02;
         // Capsule frame : offset between leg frame and 1 end of the capsule
         // Translation
     Eigen::Matrix<ADScalar, 3, 1> capsPosOffset;
+    Eigen::Matrix<double, 3, 1> fcl_capsPosOffset;
     capsPosOffset[0] = 0;
     capsPosOffset[1] = 0;
     capsPosOffset[2] = 0;
+
+    fcl_capsPosOffset[0] = 0;
+    fcl_capsPosOffset[1] = 0;
+    fcl_capsPosOffset[2] = 0;
         // Rotation
     Eigen::Quaternion<ADScalar> capsRotOffset;
+    Eigen::Quaternion<double> fcl_capsRotOffset;
     capsRotOffset.setIdentity();
+    fcl_capsRotOffset.setIdentity();
         // SE3 object
     pinocchio::SE3Tpl<ADScalar> capsFrame(capsRotOffset, capsPosOffset);
+    pinocchio::SE3Tpl<double> fcl_capsFrame(fcl_capsRotOffset, fcl_capsPosOffset);
     
 
     // Generate the code for the specified frames and capsule parameters, and compile it as library
-    ADFun genFun = tapeADFunEnd2End(rmodel, hr_lower_leg, fl_upper_leg, capsLength, capsRadius, capsFrame);
+    ADFun genFun = tapeADFunEnd2End(rmodel, frameInd1, frameInd2, capsLength, capsRadius, capsFrame);
     generateCompileCLib("end2end_" + frame1Name + frame2Name,genFun);
     // Print the C code to the console
     std::cout << "---- CODE GENERATION ----" << std::endl;
@@ -91,16 +104,24 @@ int main(int argc, char* argv[])
         // Output : distance
     Eigen::Matrix<double, Eigen::Dynamic, 1> Y_test;
     Y_test.resize(1);
-    
+
+        // FCL check (NOT 100% RELIABLE YET)
+    auto start_fcl = high_resolution_clock::now();
+    double fcl_dist = getFCLResult(rmodel,gmodel,rdata,X_test,frame1Name, frame2Name, fcl_capsLen, fcl_capsRad);//, fcl_capsFrame);
+    auto stop_fcl = high_resolution_clock::now(); 
+    auto duration_fcl = duration_cast<nanoseconds>(stop_fcl - start_fcl);
+
     // Function evaluation with start and stop timestamps
-    auto start = high_resolution_clock::now();
+    auto start_cg = high_resolution_clock::now();
     model->ForwardZero(X_test, Y_test);
-    auto stop = high_resolution_clock::now(); 
-    auto duration = duration_cast<nanoseconds>(stop - start); 
+    auto stop_cg = high_resolution_clock::now(); 
+    auto duration_cg = duration_cast<nanoseconds>(stop_cg - start_cg); 
 
     // Print output
     std::cout << "X = \n" << X_test << std::endl;
-    std::cout << "\tDist. result : " << Y_test << std::endl;
-    std::cout << "Time taken by function: " << duration.count() << " nanoseconds" << std::endl; 
+    std::cout << "\tDist. result (codegen): " << Y_test << std::endl;
+    std::cout << "Time taken by function: " << duration_cg.count() << " nanoseconds" << std::endl; 
+    std::cout << "\tDist. result (FCL): " << fcl_dist << std::endl;
+    std::cout << "Time taken by function: " << duration_fcl.count() << " nanoseconds" << std::endl;
 
 }
--- src/end2end_legs_collision_check.hpp
+++ src/end2end_legs_collision_check.hpp
@@ -1,13 +1,27 @@
 #include <iostream>
+//#include <hpp/fcl/collision_object.h>
+//#include <hpp/fcl/shape/geometric_shapes.h>
+
 #include "relative-placement-codegen.hpp"
 #include "segment-segment-distance-codegen.hpp"
+
+#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/algorithm/geometry.hpp"
+//#include "pinocchio/fcl.hpp"
 
 using namespace pinocchio;
 
 // Function to generate
 // Wrapper for pinocchio::forwardKinematics for frames f1, f2 + segmentDistance
 template<typename Scalar>
-Scalar end2endWrapper(pinocchio::ModelTpl<Scalar> model, pinocchio::DataTpl<Scalar> data, Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& config, int frameInd1, int frameInd2,Scalar capsLength,Scalar capsRadius,pinocchio::SE3Tpl<Scalar> f1Mcaps)
+Scalar end2endWrapper(pinocchio::ModelTpl<Scalar> model, 
+                      pinocchio::DataTpl<Scalar> data, 
+                      Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& config, 
+                      int frameInd1, 
+                      int frameInd2,
+                      Scalar capsLength,
+                      Scalar capsRadius,
+                      pinocchio::SE3Tpl<Scalar> f1Mcaps)
 {
     //Capsules parameters (hardcoded):
     // TODO : place and orient the capsule : SE3Tpl<Scalar> f1Mcaps;
@@ -55,6 +69,65 @@ Scalar end2endWrapper(pinocchio::ModelTpl<Scalar> model, pinocchio::DataTpl<Scal
                                          caps1Pos1[0], caps1Pos1[1], caps1Pos1[2])) - 2*capsRadius;
 
     
+}
+
+// Get the result of the FCL evaluation to test against
+double getFCLResult(pinocchio::Model& model, 
+                    pinocchio::GeometryModel& gmodel, 
+                    pinocchio::Data& data, 
+                    Eigen::Matrix<double, Eigen::Dynamic, 1>& config, 
+                    std::string frameName1, 
+                    std::string frameName2, 
+                    double capsLength,
+                    double capsRadius)
+                    //pinocchio::SE3& f1Mcaps)
+{
+    typedef boost::shared_ptr< fcl::CollisionGeometry > CollisionGeometryPtr;
+
+    // Get frames indices from the model
+    pinocchio::FrameIndex frameInd1 = model.getFrameId(frameName1);
+    pinocchio::FrameIndex frameInd2 = model.getFrameId(frameName2);
+
+    // Offset f1Mcaps to an end of the capsule
+    // Do the same for caps2
+        // Translation
+    Eigen::Matrix<double, 3, 1> capsPosOffset;
+    capsPosOffset[0] = 0;
+    capsPosOffset[1] = 0;
+    capsPosOffset[2] = -0.5*capsLength;
+        // Rotation
+    Eigen::Quaternion<double> capsRotOffset;
+    capsRotOffset.setIdentity();
+        // SE3 object
+    pinocchio::SE3 capsFrame(capsRotOffset, capsPosOffset);
+    
+    //f1Mcaps << f1Mcaps * capsFrame;
+
+    const CollisionGeometryPtr caps_geom1 (new hpp::fcl::Capsule(capsRadius, 0.5*capsLength)); // WARNING : FCL takes the capsule halfLength !
+    const CollisionGeometryPtr caps_geom2 (new hpp::fcl::Capsule(capsRadius, 0.5*capsLength)); // WARNING : FCL takes the capsule halfLength !
+
+    std::string caps1_name = std::string("caps_") + frameName1;
+    pinocchio::GeometryObject caps1_gobj(caps1_name,
+                                         frameInd1,
+                                         model.frames[frameInd1].parent,
+                                         caps_geom1, 
+                                         capsFrame);
+    pinocchio::GeomIndex caps1 = gmodel.addGeometryObject(caps1_gobj, model);
+
+    std::string caps2_name = std::string("caps_") + frameName2;  
+    pinocchio::GeometryObject caps2_gobj(caps2_name,
+                                         frameInd2,
+                                         model.frames[frameInd2].parent,
+                                         caps_geom2, 
+                                         capsFrame);
+    pinocchio::GeomIndex caps2 = gmodel.addGeometryObject(caps2_gobj, model);  
+
+    gmodel.addCollisionPair(pinocchio::CollisionPair(caps1,caps2));
+    
+    pinocchio::GeometryData gdata(gmodel);
+    pinocchio::computeDistances(model,data,gmodel,gdata,config);
+    std::vector< fcl::DistanceResult > collisions_dist = gdata.distanceResults;              
+    return collisions_dist[gmodel.findCollisionPair(pinocchio::CollisionPair(caps1,caps2))].min_distance;                                                                                                                                      
 }
 
 // Generates the model for the function f(q, f1, f2) = dist. between legs f1,f2 
