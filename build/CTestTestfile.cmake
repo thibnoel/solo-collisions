2c22371467be0b330ab7787dfdfa127ba4ff39f4
Thibault Noel <thb.noel@gmail.com>
2020-09-22 13:07:43 +0200

    remove build folder from repo

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tnoel/stage/solo-collisions
-# Build directory: /home/tnoel/stage/solo-collisions/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tnoel/stage/solo-collisions/src
-# Build directory: /home/tnoel/stage/solo-collisions/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
