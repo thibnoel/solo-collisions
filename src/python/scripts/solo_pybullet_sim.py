e2d9cd665d71d487e769f32b77393effa0a09b90
LAAS-PC <thibault.noel@laas.fr>
2020-09-28 17:09:13 +0200

    clearing and regrouping example python scripts

--- src/python/scripts/solo_pybullet_sim.py
+++ src/python/scripts/solo_pybullet_sim.py
@@ -9,10 +9,26 @@ import pybullet as pb
 import pybullet_data
 import pybullet_utils.bullet_client as bc
 
-from solo12_collision_gradient import computeDistJacobian
-from shoulder_approx.solo_shoulder_approx_torch_nn import *
-from solo_collisions_avoidance_control.solo_control import *
-from solo_collisions_avoidance_control.solo_c_wrappers import *
+#from solo12_collision_gradient import computeDistJacobian
+#from shoulder_approx.solo_shoulder_approx_torch_nn import *
+from solo_collisions_avoidance_control.solo_initialization import *
+
+from solo_collisions_avoidance_control.solo_coll_wrapper_pytorch import *
+from solo_collisions_avoidance_control.solo_coll_wrapper_fcl import *
+from solo_collisions_avoidance_control.solo_coll_wrapper_c import *
+from solo_collisions_avoidance_control.collisions_controller import *
+#from solo_collisions_avoidance_control.solo_c_wrappers import *
+
+def computePDTorque(q, q_des, vq, Kp, Kv, q_ind=[]):
+    if len(q_ind) == 0 :
+        q_ind = [i for i in range(len(q))]
+    tau_q = np.zeros(len(q))
+    tau_q[q_ind] = -Kp*(q[q_ind] - q_des[q_ind]) - Kv*vq[q_ind]
+    return tau_q
+
+
+def computeAcceleration(M, b, tau):
+    return np.linalg.inv(M)@(tau - b) 
 
 
 def getPosVelJoints(robotID, revoluteJointIndices, physicsClient):
@@ -42,11 +58,11 @@ def loadBulletPhysicsClient(dt, enableGUI=True, grav=-9.81):
     physicsClient.setAdditionalSearchPath("/opt/openrobots/share/example-robot-data/robots/solo_description")
     physicsClient.setAdditionalSearchPath("/home/tnoel/stage/solo-collisions/urdf")
 
-    #robotID = physicsClient.loadURDF("solo12_simplified_bullet.urdf", robotStartPos, robotStartOrientation, useFixedBase=1)
-    robotID = physicsClient.loadURDF("solo8_simplified_bullet.urdf", robotStartPos, robotStartOrientation, useFixedBase=1)
+    robotID = physicsClient.loadURDF("solo12_simplified_bullet.urdf", robotStartPos, robotStartOrientation, useFixedBase=1)
+    #robotID = physicsClient.loadURDF("solo8_simplified_bullet.urdf", robotStartPos, robotStartOrientation, useFixedBase=1)
     # Initialize velocity control 
-    #revoluteJointIndices = [0,1,2,4,5,6,8,9,10,12,13,14]
-    revoluteJointIndices = [0,1,3,4,6,7,9,10]
+    revoluteJointIndices = [0,1,2,4,5,6,8,9,10,12,13,14]
+    #revoluteJointIndices = [0,1,3,4,6,7,9,10]
     
     physicsClient.setJointMotorControlArray(robotID,
                                             jointIndices = revoluteJointIndices,
@@ -68,7 +84,7 @@ dt = 1e-3
 grav = -9.81
 robotID, revoluteJointIndices, physicsClient = loadBulletPhysicsClient(dt, enableGUI=True)
 
-robot, rmodel, rdata, gmodel, gdata = initSolo(solo=True)
+robot, rmodel, rdata, gmodel, gdata = initSolo(solo=False)
 aq0 = np.zeros(robot.model.nv)
 
 #trainedModel_path = "/home/tnoel/stage/solo-collisions/src/python/pytorch_data/test_2Dmodel_481.pth"
@@ -82,13 +98,14 @@ min_dist = 10*np.ones(6)
 
 
 # Gen code binding test
-#so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_mod.so"
-#nn_so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_nn.so"
-#cCollFun = CDLL(so_file)
-#nnCCollFun = CDLL(nn_so_file)
-
-so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_legs8.so"
+so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_mod.so"
+nn_so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_nn.so"
 cCollFun = CDLL(so_file)
+nnCCollFun = CDLL(nn_so_file)
+
+#so_file = "/home/tnoel/stage/solo-collisions/compiled_c_lib/libcoll_legs8.so"
+#cCollFun = CDLL(so_file)
+avg_exec_time = 0
 
 print(physicsClient.getNumJoints(robotID))
 for k in range(300):    
@@ -110,24 +127,24 @@ for k in range(300):
     # Initialize dist log
     dist = 5*np.zeros(len(gmodel.collisionPairs) + 4)
     #dist = np.zeros(6)
-    '''
+    
     ##################### SOLO 12
     # Get results for the legs from FCL
     legs_dist, Jlegs, legs_pairs = compute_legs_Jdist_avoidance(q, rmodel, rdata, gmodel, gdata)
     # Get results for the shoulders from a trained neural network
     shoulders_dist, Jshd, shoulders_pairs = compute_shoulders_Jdist_avoidance(q, shoulder_model, rmodel, rdata, gmodel, gdata, characLength=0.34)
 
-    ### Get results from C generated code
+    start = timer()
+    ### Get results from C generated code (legs)
     c_results = getLegsCollisionsResults(q, cCollFun, 12,20)
     c_dist_legs = getLegsDistances(c_results,12,20)
     c_Jlegs = getLegsJacobians(c_results,12,20)
-    # Currently only 1 shoulder from gen. code 
-    c_shoulder_FL = getShoulderCollisionsResults(q[0:2], nnCCollFun, 2)
-    c_FLsh_dist = getShoulderDistance(c_shoulder_FL)
-    c_JFLsh = getShoulderJacobian(c_shoulder_FL)
+    ### Get results from C generated code (shoulder neural net)
+    c_shd_dist, c_shd_jac = getAllShouldersCollisionsResults(q, nnCCollFun, 2, offset=0.08)
+    end = timer()
+    avg_exec_time += end-start
+    print("Avg exec time : {:.6f}\r".format(avg_exec_time/(k+1)))
 
-    legs_dist = c_dist_legs
-    Jlegs = c_Jlegs
     
     thresh_legs = 0.05
     kp_legs = 25
@@ -138,7 +155,7 @@ for k in range(300):
     kv_shd = 0.1
 
     tau_q += computeRepulsiveTorque(q, vq, c_dist_legs, c_Jlegs, thresh_legs, kp_legs, kv_legs)
-    tau_q += computeRepulsiveTorque(q, vq, shoulders_dist, Jshd, thresh_shd, kp_shd, kv_shd)
+    tau_q += computeRepulsiveTorque(q, vq, c_shd_dist, c_shd_jac, thresh_shd, kp_shd, kv_shd)
     
     for i in range(len(legs_pairs)):
         for j in range(len(gmodel.collisionPairs)):
@@ -165,6 +182,8 @@ for k in range(300):
     kp_legs = 10
     kv_legs = 0.02
 
+    emergency_d_thresh = thresh_legs/4
+
     # Compute torque and measure elapsed time
     start = timer()
     tau_q += computeRepulsiveTorque(q, vq, c_dist_legs, c_Jlegs, thresh_legs, kp_legs, kv_legs)
@@ -176,7 +195,7 @@ for k in range(300):
         dist[i] = c_dist_legs[i]
         if(min_dist[i] > c_dist_legs[i]):
             min_dist[i] = c_dist_legs[i]
-        
+    '''
     
     #tau_q += tau_rep
 
---
+++ src/python/scripts/talos_shoulder_coll_sampling.py
@@ -0,0 +1,112 @@
+from example_robot_data import loadTalos
+from collision_sampling.articular_space_coll_sampling import *
+from collision_sampling.articular_space_coll_visualization import *
+
+def initTalos():
+    robot = loadTalos()
+    # Get robot model, data, and collision model
+    rmodel = robot.model
+    rdata  = rmodel.createData()
+    gmodel = robot.collision_model
+
+    geom_names = ["torso_2_link_0",
+                    #"arm_left_1_link_0",
+                    "arm_left_2_link_0",
+                    "arm_left_3_link_0",
+                    "arm_left_4_link_0",
+                    "arm_left_5_link_0"]
+                    #"arm_left_6_link_0",
+                    #"arm_left_7_link_0"]
+    geom_IDs = [gmodel.getGeometryId(name) for name in geom_names]
+
+    n_geom = len(geom_IDs)
+    npairs = 0
+    '''
+    for i in range(n_geom):
+        for j in range(i+1, n_geom):
+            p = pio.CollisionPair(geom_IDs[i], geom_IDs[j])
+            gmodel.addCollisionPair(p)
+    '''
+    gmodel.addCollisionPair(pio.CollisionPair(geom_IDs[0], geom_IDs[1]))
+    gmodel.addCollisionPair(pio.CollisionPair(geom_IDs[0], geom_IDs[2]))
+    gmodel.addCollisionPair(pio.CollisionPair(geom_IDs[0], geom_IDs[3]))
+    gmodel.addCollisionPair(pio.CollisionPair(geom_IDs[0], geom_IDs[4]))
+
+    npairs = len(gmodel.collisionPairs)
+
+    gdata = gmodel.createData()
+    return robot, rmodel, rdata, gmodel, gdata, geom_names, npairs
+
+
+##### MAIN #####
+if __name__ == "__main__":
+    robot, rmodel, rdata, gmodel, gdata, geom_names, npairs = initTalos()
+    robot_config = robot.q0
+    robot_config[23] += 0.8
+
+    enableGUI = False
+
+    if(enableGUI):
+        robot.initViewer(loadModel=True)
+        gv = robot.viewer.gui
+        # Display the robot
+        robot.rebuildData() 
+        robot.displayCollisions(True)
+        robot.displayVisuals(False)
+        robot.display(robot_config) 
+        for n in gv.getNodeList():
+            print(n)
+            if 'collision' in n and len(n)>27:
+                gv.setVisibility(n,'ON')
+                gv.setColor(n, [1,1,1,1])
+            for g in geom_names:
+                if(g in n):
+                    #gv.setVisibility(n,'OFF')
+                    gv.setColor(n, [1,0.5,0,1])
+        gv.refresh()
+
+    # Initialize joints ranges to sample
+    q0_range = [0 - 2.4, 2*np.pi - 2.4]
+    q1_range = [0 - 2.4, 2*np.pi - 2.4]
+    q2_range = [0, 2*np.pi]
+
+    # Initialize sampling parameters
+    q_ind = [21,22,24]
+    q_ranges = [q0_range, q1_range, q2_range]
+    q_steps = [50,50, 9]
+
+    # Sample FCL distance
+    grid_config = generateGridConfigs(q_ind, q_ranges, q_steps)
+    col_map = sampleFCLDistanceFromConfigList(robot_config, q_ind, grid_config, [k for k in range(npairs)], rmodel, rdata, gmodel, gdata, computeDist=False)
+
+    # Sample collision boundary
+    bound = boundaryRandomSapling(q_ind, q_ranges, 1000, 1e-9, robot_config, [k for k in range(npairs)], rmodel, rdata, gmodel, gdata, extend_periodic=True)
+
+    # Convert distance to articular distance
+    dist_metric='euclidean'
+    articular_col_map = spatialToArticular(col_map, bound, batch_size=100, metric=dist_metric)
+    articular_jacobian = getSampledJac(articular_col_map, bound, batch_size=100, metric=dist_metric)
+
+    col_map = articular_col_map
+
+    ######## Results visualization
+    # 3D visualization
+    fig = plt.figure()
+    visualize3DData(fig, col_map[np.where(col_map[:,-1] > 0)], q_ind, q_ranges, grid=False, q_steps=q_steps, subplot_pos=211, title="Dist. > 0", cmap=plt.cm.viridis)
+    visualize3DData(fig, col_map[np.where(col_map[:,-1] <= 0)], q_ind, q_ranges, grid=False, q_steps=q_steps, subplot_pos=212, title="Dist. <= 0", cmap=plt.cm.viridis)
+    
+    # 2D flattened visualization
+    plt.figure()
+    visualizeFlat3DData(articular_col_map, q_ind, q_ranges, q_steps, title="FCL distance", cmap=plt.cm.RdYlGn)
+
+    jac0 = articular_jacobian[:,[0,1,2,3]]
+    jac1 = articular_jacobian[:,[0,1,2,4]]
+    jac2 = articular_jacobian[:,[0,1,2,5]]
+
+    # Jacobians visualization
+    fig2 = plt.figure()
+    visualize3DData(fig2, jac0, q_ind, q_ranges, grid=False, q_steps=q_steps, subplot_pos=131, title="J_q0", cmap=plt.cm.PiYG)
+    visualize3DData(fig2, jac1, q_ind, q_ranges, grid=False, q_steps=q_steps, subplot_pos=132, title="J_q1", cmap=plt.cm.PiYG)
+    visualize3DData(fig2, jac2, q_ind, q_ranges, grid=False, q_steps=q_steps, subplot_pos=133, title="J_q2", cmap=plt.cm.PiYG)
+
+    plt.show()
