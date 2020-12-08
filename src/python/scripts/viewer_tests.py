2022bc1a4d7ea36d2370e1d2dc3bba686d21268e
GCP-machine <thibault@basic-ubuntu.europe-west2-c.c.projets-perso-gcp.internal>
2020-12-08 11:28:35 +0000

    clean python code (delete outdated/useless scripts)

--- src/python/scripts/viewer_tests.py
+++
@@ -1,6 +0,0 @@
-from solo_collisions_avoidance_control.solo_initialization import *
-from solo_collisions_avoidance_control.solo_coll_wrapper_c import *
-from solo_collisions_avoidance_control.collisions_controller import *
-
-robot, rmodel, rdata, gmodel, gdata = initSolo(solo=False)
-robot.initViewer(True, viewer=gui)
